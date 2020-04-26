194dd4921f936e2b61d74c646ed6df33c3aee4e4
Haider <hai.tiwana@gmail.com>
2020-04-26 12:40:01 -0500

    inital commit

---
+++ array-and-string-manipulation/merging-meeting-times/merging-meeting-times.js
@@ -0,0 +1,35 @@
+const mergeRanges = (meetings) => {
+  const meetingsCopy = JSON.parse(JSON.stringify(meetings));
+  const sortedMeetings = meetingsCopy.sort(
+    (a, b) => a['startTime'] - b['startTime']
+  );
+  const mergedMeetings = [sortedMeetings[0]];
+
+  for (let i = 1; i < sortedMeetings.length; i++) {
+    const currentMeeting = sortedMeetings[i];
+    const lastMergedMeeting = mergedMeetings[mergedMeetings.length - 1];
+    // check to see if they can be merged
+    if (lastMergedMeeting.endTime >= currentMeeting.startTime) {
+      const endTime = Math.max(
+        currentMeeting.endTime,
+        lastMergedMeeting.endTime
+      );
+      lastMergedMeeting.endTime = endTime;
+    } else {
+      mergedMeetings.push(currentMeeting);
+    }
+  }
+
+  return mergedMeetings;
+};
+
+const meetingTimes = [
+  { startTime: 0, endTime: 1 },
+  { startTime: 3, endTime: 5 },
+  { startTime: 4, endTime: 8 },
+  { startTime: 10, endTime: 12 },
+  { startTime: 9, endTime: 10 },
+];
+
+// console.log({ meetingRange });
+console.log(mergeRanges(meetingTimes));
